c54cb569b2523e62e5b2078620c540a94c5f1ec4
Henry Zeng <youhengzeng8@gmail.com>
2024-11-12 11:46:31 -0500

    create MyCar class, add basic product information

---
+++ main.js
@@ -0,0 +1,9 @@
+function MyCar(config) {
+  // Basic product information
+  this.manufacturer = 'Greenlight'
+  this.modelNumber = '30166/48'
+  this.make = 'Ford'
+  this.model = 'Mustang'
+  this.year = 1979
+  this.scale = '1:64'
+}
