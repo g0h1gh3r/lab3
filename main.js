d686c78978df30ae8cc8da06c3ba7e7e0f5ce7d4
Henry Zeng <youhengzeng8@gmail.com>
2024-11-12 11:48:51 -0500

    add physical specification and collector information

--- main.js
+++ main.js
@@ -9,4 +9,17 @@ function MyCar(config) {
   // Event and historical information
   this.event = '63rd Annual Indianapolis 500 Mile Race'
   this.colors = ['White', 'Black']
+  // Physical specifications
+  this.material = 'Diecast Metal'
+  this.series = 'Indianapolis 500 Official Pace Cars'
+  this.features = [
+    'Detailed Exterior',
+    'Official Indy 500 Graphics',
+    'Authentic Paint Scheme',
+  ]
+  // Collector information
+  this.packaging = 'Blister Pack'
+  this.limitedEdition = true
+  this.productionRun = 4800
+  this.basePrice = 29.99
 }
